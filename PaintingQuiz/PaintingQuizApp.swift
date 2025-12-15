78c1800930c33839b329b2cbba2e8d78b07eaa95
Gilang Ramadhan <148070652+Kai-De-Paxley@users.noreply.github.com>
2025-12-15 10:11:02 +0700

    Initial Commit

---
+++ PaintingQuiz/PaintingQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintingQuizApp.swift
+//  PaintingQuiz
+//
+//  Created by Gilang Ramadhan on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PaintingQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
